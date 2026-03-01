08deb5f70c490d26d34b632cb1694d28e8c3a6e8
xiao-e-yun <a0970323059@gmail.com>
2026-03-01 16:49:21 +0800

    refactor: remove feature methods

--- src/manager/mod.rs
+++ src/manager/mod.rs
@@ -248,60 +248,6 @@ where
     pub fn bind<Id: BindableId>(&self, id: Id) -> Binded<'_, Id, C> {
         Binded::new(self, id)
     }
-
-    /// Returns this archive's feature value by name.
-    pub fn get_feature(&self, name: &str) -> Result<i64, rusqlite::Error> {
-        self.conn()
-            .query_row("SELECT value FROM features WHERE name = ?", [name], |row| {
-                row.get(0)
-            })
-            .optional()
-            .transpose()
-            .unwrap_or(Ok(0))
-    }
-
-    /// Returns this archive's feature value and extra data by name.
-    pub fn get_feature_with_extra(
-        &self,
-        name: &str,
-    ) -> Result<(i64, HashMap<String, Value>), rusqlite::Error> {
-        self.conn()
-            .query_row(
-                "SELECT value, extra FROM features WHERE name = ?",
-                [name],
-                |row| {
-                    let value: i64 = row.get(0)?;
-                    let extra: String = row.get(1)?;
-                    let extra: HashMap<String, Value> =
-                        serde_json::from_str(&extra).unwrap_or_default();
-                    Ok((value, extra))
-                },
-            )
-            .optional()
-            .transpose()
-            .unwrap_or(Ok((0, HashMap::default())))
-    }
-
-    /// Set feature value by name.
-    pub fn set_feature(&self, name: &str, value: i64) {
-        self.conn()
-            .execute(
-                "INSERT INTO features (name, value) VALUES (?, ?) ON CONFLICT(name) DO UPDATE SET value = ?",
-                params![name, value, value],
-            )
-            .unwrap();
-    }
-
-    /// Set feature value and extra data by name.
-    pub fn set_feature_with_extra(&self, name: &str, value: i64, extra: HashMap<String, Value>) {
-        let extra = serde_json::to_string(&extra).unwrap();
-        self.conn()
-            .execute(
-                "INSERT OR REPLACE INTO features (name, value, extra) VALUES (?, ?, ?)",
-                params![name, value, extra],
-            )
-            .unwrap();
-    }
 }
 
 /// Trait for types that can provide a database connection
